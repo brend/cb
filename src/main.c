f9ee36967264329bc7d946bc8d78bc24161b7104
Philipp Brendel <waldrumpus@gmail.com>
2023-09-22 09:35:36 +0200

    read from stdin

--- src/main.c
+++ src/main.c
@@ -10,6 +10,7 @@ void evaluate_expression(char *expression);
 void evaluate_file(char *filename);
 void evaluate_file_ptr(FILE *file);
 void evaluate_stdin();
+char *read_stdin_to_end();
 
 int main(int argc, char** argv) {
 	if (argc < 1) {
@@ -24,12 +25,32 @@ int main(int argc, char** argv) {
 	}
 
 	if (argc == 1) {
-		evaluate_stdin();
+    char *expression = read_stdin_to_end();
+		evaluate_expression(expression);
+    free(expression);
 		return 0;
 	}
 
 	char *filename = argv[1];
 	evaluate_file(filename);
+}
+
+
+char *read_stdin_to_end() {
+  const size_t CHUNK_SIZE = 1024;
+  char *buffer = NULL;
+  size_t buffer_size = 0;
+  size_t bytes_read = 0;
+  size_t total_bytes_read = 0;
+
+  do {
+    buffer_size += CHUNK_SIZE;
+    buffer = realloc(buffer, buffer_size);
+    bytes_read = fread(buffer + total_bytes_read, 1, CHUNK_SIZE, stdin);
+    total_bytes_read += bytes_read;
+  } while (bytes_read == CHUNK_SIZE);
+
+  return buffer;
 }
 
 void evaluate_expression(char *expression) {
